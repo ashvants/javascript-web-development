7bd9d336478299d55b2b4ffbc6e5a2bb673420af
ashvants <44884742+ashvants@users.noreply.github.com>
2020-01-31 16:56:30 +1100

    completed automatic sprite generation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 svgSprite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
 
 var config = {
     mode: {
         css: {
+            sprite: 'sprite.svg',
            render: {
                css: {
                     template: './gulp/templates/sprite.css'
@@ -14,11 +16,22 @@ var config = {
     }
 }
 
+gulp.task('beginClean', function() {
+    return del(['./app/temp/sprite', './app/assets/images/sprites'])
+
+});
+
 gulp.task('createSprite', function() {
     return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite'));
 
+
+});
+
+gulp.task('copySpriteGraphic', function() {
+    return gulp.src('./app/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('app/assets/images/sprites'));
 
 });
 
@@ -29,4 +42,11 @@ gulp.task('copySpriteCSS', function(){
 
 });
 
+gulp.task ('endClean', function() {
+    return del('./app/temp/sprite');
+});
 
+
+gulp.task('icons', 
+                    (gulp.series('beginClean', 'createSprite','copySpriteGraphic', 'copySpriteCSS', 'endClean'))
+);
