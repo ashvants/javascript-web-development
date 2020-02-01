3b13478238e3c14f5aad234c8ba63594f510d9a5
ashvants <44884742+ashvants@users.noreply.github.com>
2020-02-02 08:24:27 +1100

    header changes

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,9 +5,10 @@ cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
 mixins = require('postcss-mixins');
+hexrgba = require('postcss-hexrgba')
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
       .pipe(gulp.dest('./app/temp/styles'));
 });
