b52e1162e7e3b58cd1e8eb9f25ed8f7d83e41e10
ashvants <44884742+ashvants@users.noreply.github.com>
2020-02-18 17:54:05 +1100

    fixed headers

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,11 +1,13 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader'
 
 
 
 
 var mobileMenu = new MobileMenu();
+var stickyHeader = new StickyHeader();
 
 new RevealOnScroll($(".feature-item"), "85%");
-new RevealOnScroll($(".testimonial"), "60%");
+new RevealOnScroll($(".testimonial"), "70%");
